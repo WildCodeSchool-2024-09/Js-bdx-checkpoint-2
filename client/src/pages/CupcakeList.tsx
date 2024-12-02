cc26563afb17a91770423e9b8da138d602f0f004
Sarah Smandack <sarah.smandack@gmail.com>
2024-12-02 10:24:10 +0100

    finished step 2

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -40,7 +40,8 @@ type CupcakeArray = typeof sampleCupcakes;
 
 function CupcakeList() {
   // Step 1: get all cupcakes
-  console.info(useLoaderData() as CupcakeArray);
+  const cupcakes = useLoaderData() as CupcakeArray;
+  console.info(cupcakes);
 
   // Step 3: get all accessories
 
@@ -62,9 +63,15 @@ function CupcakeList() {
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
         {/* Step 5: filter cupcakes before repeating */}
-        <li className="cupcake-item">
-          <Cupcake data={sampleCupcakes[0]} />
-        </li>
+        {cupcakes.length === 0 ? (
+          <p>No cupcakes available.</p>
+        ) : (
+          cupcakes.map((cupcake) => (
+            <li key={cupcake.id} className="cupcake-item">
+              <Cupcake data={cupcake} />
+            </li>
+          ))
+        )}
         {/* end of block */}
       </ul>
     </>
